b7f4d5759943be3651dc33bb8b468ceb0af1b0cd
johnnyb8 <barry@DESKTOP-EBQ3G8L.localdomain>
2018-02-22 19:08:35 -0330

    Adding project with working activities

---
+++ app/src/main/java/ca/mun/engi5895/stargazer/MainActivity.java
@@ -0,0 +1,32 @@
+package ca.mun.engi5895.stargazer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void geoGo(View view) {
+        Intent intent = new Intent(this, GeocentricActivity.class);
+        startActivity(intent);
+    }
+
+    public void helioGo(View view) {
+        Intent intent = new Intent(this, HeliocentricActivity.class);
+        startActivity(intent);
+    }
+
+    public void settingsGo(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+
+}
