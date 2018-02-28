70164126e6bf92a3177cfeb06ca26ccc3d2c5e1c
noahgallant-MUN <noahgallant@pei.sympatico.ca>
2018-02-28 18:39:53 -0330

    Added Entity Class

--- app/src/main/java/ca/mun/engi5895/stargazer/Entity.java
+++ app/src/main/java/ca/mun/engi5895/stargazer/Entity.java
@@ -3,12 +3,21 @@ package ca.mun.engi5895.stargazer;
 /**
  * Created by noahg on 2/28/2018.
  */
+import android.nfc.TagLostException;
+import java.util.calendar;
 import org.orekit.propagation.analytical.tle;
 
 public class Entity {
 
-    Entity()j{
+    int avgMass = 200;
+    Entity(){
 
+        TLE entity = new TLE(string line1, string line2);
+
+        TimeScale timeZone = new UTCScale();
+        Calendar date = new Calendar()
+        AbsoluteDate initialDate = new AbsoluteDate(year, month, day, hour, min, sec, timeZone);
+        TLEPropagator calc = new TLEPropagator(entity, ,avgMass);
 
 
     }
