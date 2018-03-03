ab5682d94b7ad4e93056ae6bca40bc755554e8ab
noahgallant-MUN <noahgallant@pei.sympatico.ca>
2018-03-03 15:47:33 -0330

    Continued work on constructor

--- app/src/main/java/ca/mun/engi5895/stargazer/Entity.java
+++ app/src/main/java/ca/mun/engi5895/stargazer/Entity.java
@@ -7,9 +7,12 @@ import android.nfc.TagLostException;
 
 import org.orekit.attitudes.Attitude;
 import org.orekit.attitudes.AttitudeProvider;
+import org.orekit.errors.OrekitException;
 import org.orekit.frames.FramesFactory;
+import org.orekit.orbits.PositionAngle;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
+import org.orekit.propagation.conversion.TLEPropagatorBuilder;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
@@ -17,7 +20,10 @@ import org.orekit.time.UTCScale;
 import org.orekit.attitudes.BodyCenterPointing;
 import org.orekit.frames.Frame;
 import org.orekit.bodies.Ellipsoid;
-
+import org.orekit.utils.PVCoordinates;
+import org.orekit.utils.AngularCoordinates;
+import org.orekit.propagation.analytical.KeplerianPropagator;
+import org.orekit.orbits.PositionAngle;
 
 import java.util.Date;
 import java.util.Calendar;
@@ -29,7 +35,8 @@ public class Entity {
     int avgMass = 200;
     TLE entity;
     Calendar calendar;
-    TLEPropagator calc;
+    Attitude att;
+    PVCoordinates currentPV;
 
     Entity(String line1, String line2){
 
@@ -40,13 +47,14 @@ public class Entity {
         calendar = GregorianCalendar.getInstance();
         calendar.setTime(date);
         Frame frame = FramesFactory.getGCRF();
+        TLEPropagatorBuilder builder = new TLEPropagatorBuilder(entity, MEAN,1);
 
         Ellipsoid el = new Ellipsoid(frame, 1,1,1);
-
+        AngularCoordinates orientation = new AngularCoordinates();
         AbsoluteDate initialDate = new AbsoluteDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND), timeZone);
-
-        Attitude att = AttitudeProvider( , date, frame);
-        calc = new TLEPropagator(entity, att,avgMass);
+        currentPV = new PVCoordinates();
+        att = new Attitude(initialDate, frame, orientation);
+        KeplerianPropagator tleProp = new TLEPropagator(entity, att, avgMass);
 
     }
 
