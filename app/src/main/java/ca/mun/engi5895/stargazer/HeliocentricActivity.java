18b8b939db8d0728127bd5a91fb83568d0b6ee2f
noahgallant-MUN <noahgallant@pei.sympatico.ca>
2018-03-28 23:12:28 -0230

    Fixed Date Bug

--- app/src/main/java/ca/mun/engi5895/stargazer/HeliocentricActivity.java
+++ app/src/main/java/ca/mun/engi5895/stargazer/HeliocentricActivity.java
@@ -350,7 +350,7 @@ public class HeliocentricActivity extends AppCompatActivity {
         Date date = new Date(); //creates date
         Calendar calendar = GregorianCalendar.getInstance(); //sets calendar
         calendar.setTime(date); //updates date and time
-        AbsoluteDate abDate = new AbsoluteDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND), timeZone); //creates orekit absolute date from calender
+        AbsoluteDate abDate = new AbsoluteDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND), timeZone); //creates orekit absolute date from calender
         return body.getPVCoordinates(abDate, CelestialBodyFactory.getSun().getBodyOrientedFrame());
     }
 
